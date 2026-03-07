c27511f4f07b98457057f1bfaebdab306ef1942a
Saqlain Shah <codewithsqln@gmail.com>
2026-03-07 16:17:55 +0000

    React+TailwindCSS Project

---
+++ ecommercePage/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <button className="bg-blue-500 text-white px-4 py-2 rounded">
+        Click Me
+      </button>
+    </div>
+  )
+}
+
+export default App
